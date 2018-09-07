43399cd524f89e0f54197523c30460f5c6c1dea9
Orsolya Lévai <solya0082@gmail.com>
2018-09-07 16:30:33 +0200

    Modified print transaction

---
+++ main/com/supercharge/bank/util/PrintDeposit.java
@@ -0,0 +1,9 @@
+package com.supercharge.bank.util;
+
+public class PrintDeposit implements PrintTransaction {
+
+    @Override
+    public void printTransactions() {
+        
+    }
+}
--- main/com/supercharge/bank/util/PrintTransaction.java
+++ main/com/supercharge/bank/util/PrintTransaction.java
@@ -1,22 +1,7 @@
 package com.supercharge.bank.util;
 
-import com.supercharge.bank.model.Client;
-import com.supercharge.bank.transaction.Transaction;
 
-public class PrintTransaction {
-    private Client client;
+public interface PrintTransaction {
 
-    public PrintTransaction(Client client) {
-        this.client = client;
-    }
-
-    public void printTransactions() {
-        for (Transaction transaction: client.getTransactionHistory()) {
-            System.out.println("Name: " + client.getFirstName() + " " + client.getLastName());
-            System.out.println("Transaction " + transaction.getTransactionId() + ": ");
-            System.out.println("Transaction type:" + transaction.getTransactionType().name());
-
-            //if (transaction.getTransactionType().equals())
-        }
-    }
+    void printTransactions();
 }
